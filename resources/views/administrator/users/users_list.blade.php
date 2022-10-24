6b8a3bd642b5ca72b7ddbf733d07e0c2b0fdc257
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2022-10-24 12:21:11 +0700

    Add kontribusi field on users list

--- resources/views/administrator/users/users_list.blade.php
+++ resources/views/administrator/users/users_list.blade.php
@@ -4,70 +4,75 @@
     <div class="container">
         <h2 class="font-weight-bold" style="color: #FF357C!important;">Daftar Pengguna</h2>
 
-        <hr/>
+        <hr />
 
         <div class="row justify-content-center">
             <div class="col-md-12 mt-2">
                 <div class="table-responsive mt-2">
                     <table class="table table-bordered table-hover">
                         <thead>
-                        <tr class="text-center text-white" style="background-color: #FF357C!important;">
-                            <th class="align-middle">Nama</th>
-                            <th class="align-middle">Email</th>
-                            <th class="align-middle">Administrator</th>
-                            <th class="align-middle">Aksi</th>
-                        </tr>
+                            <tr class="text-center text-white" style="background-color: #FF357C!important;">
+                                <th class="align-middle">Nama</th>
+                                <th class="align-middle">Email</th>
+                                <th class="align-middle">Kontribusi</th>
+                                <th class="align-middle">Administrator</th>
+                                <th class="align-middle">Aksi</th>
+                            </tr>
                         </thead>
 
                         <tbody>
-                        @foreach($users as $user)
-                            <tr class="text-center">
-                                <td class="align-middle">{{ $user->name }}</td>
-                                <td class="align-middle"><span class="text-nowrap">{{ $user->email }}</span></td>
-                                <td class="align-middle">
-                                    <div class="d-flex justify-content-center">
-                                        @if(UserDetails::where('email', $user->email)->first()->is_administrator == true)
-                                            <span class="text-center">v</span>
-                                        @else
-                                            <span class="text-center">x</span>
-                                        @endif
-                                    </div>
-                                </td>
-                                <td class="align-middle">
-                                    <div class="d-flex justify-content-center">
-                                        <div class="dropdown">
-                                            <button class="btn btn-secondary dropdown-toggle" type="button"
-                                                    id="dropdownActionButton" data-toggle="dropdown"
-                                                    aria-haspopup="true" aria-expanded="false">
-                                                Pilih aksi
-                                            </button>
-
-                                            <div class="dropdown-menu" aria-labelledby="dropdownActionButton">
-                                                <button type="button" class="dropdown-item"
-                                                        data-toggle="modal"
-                                                        data-target="#resetPassword"
-                                                        data-email="{{ $user->email }}">
-                                                    Reset password
+                            @foreach ($users as $user)
+                                <tr class="text-center">
+                                    <!-- Nama -->
+                                    <td class="align-middle">{{ $user->name }}</td>
+                                    <!-- Email -->
+                                    <td class="align-middle"><span class="text-nowrap">{{ $user->email }}</span></td>
+                                    <!-- Kontribusi -->
+                                    <td class="align-middle">
+                                        {{ count(ImageLabel::where('email', $user->email)->get()) }}
+                                    </td>
+                                    <!-- Administrator -->
+                                    <td class="align-middle">
+                                        <div class="d-flex justify-content-center">
+                                            @if (UserDetails::where('email', $user->email)->first()->is_administrator == true)
+                                                <span class="text-center">v</span>
+                                            @else
+                                                <span class="text-center">x</span>
+                                            @endif
+                                        </div>
+                                    </td>
+                                    <!-- Aksi -->
+                                    <td class="align-middle">
+                                        <div class="d-flex justify-content-center">
+                                            <div class="dropdown">
+                                                <button class="btn btn-secondary dropdown-toggle" type="button"
+                                                    id="dropdownActionButton" data-toggle="dropdown" aria-haspopup="true"
+                                                    aria-expanded="false">
+                                                    Pilih aksi
                                                 </button>
 
-                                                @if(UserDetails::where('email', $user->email)->first()->email != Auth::user()->email)
-                                                    <button type="button" class="dropdown-item"
-                                                            data-toggle="modal"
-                                                            data-target="#changeUserRole"
-                                                            data-email="{{ $user->email }}">
-                                                        @if(UserDetails::where('email', $user->email)->first()->is_administrator == true)
-                                                            Jadikan sebagai pengguna biasa
-                                                        @else
-                                                            Jadikan sebagai administrator
-                                                        @endif
+                                                <div class="dropdown-menu" aria-labelledby="dropdownActionButton">
+                                                    <button type="button" class="dropdown-item" data-toggle="modal"
+                                                        data-target="#resetPassword" data-email="{{ $user->email }}">
+                                                        Reset password
                                                     </button>
-                                                @endif
+
+                                                    @if (UserDetails::where('email', $user->email)->first()->email != Auth::user()->email)
+                                                        <button type="button" class="dropdown-item" data-toggle="modal"
+                                                            data-target="#changeUserRole" data-email="{{ $user->email }}">
+                                                            @if (UserDetails::where('email', $user->email)->first()->is_administrator == true)
+                                                                Jadikan sebagai pengguna biasa
+                                                            @else
+                                                                Jadikan sebagai administrator
+                                                            @endif
+                                                        </button>
+                                                    @endif
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endforeach
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -94,8 +99,7 @@
                 <div class="modal-body">
                     <p>Apakah Anda yakin untuk melanjutkan? Silakan masukkan password Anda untuk melanjutkan.</p>
                     <form id="reset-user-password-form" action="{{ route('administrator.reset.user.password') }}"
-                          method="post"
-                          enctype="multipart/form-data">
+                        method="post" enctype="multipart/form-data">
                         @csrf
 
                         <input type="hidden" id="userEmail" name="userEmail">
@@ -108,8 +112,8 @@
 
                             <div class="col-md-10">
                                 <input id="password" name="password" type="password"
-                                       class="form-control @error('password') is-invalid @enderror"
-                                       placeholder="Masukkan password Anda">
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    placeholder="Masukkan password Anda">
 
                                 <span class="invalid-feedback" role="alert">
                                     {{ $errors->first('password') }}
@@ -122,7 +126,7 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
                     <button type="button" class="btn btn-warning"
-                            onclick="event.preventDefault(); document.getElementById('reset-user-password-form').submit();">
+                        onclick="event.preventDefault(); document.getElementById('reset-user-password-form').submit();">
                         Lanjutkan
                     </button>
                 </div>
@@ -146,8 +150,7 @@
                     <p>Apakah Anda yakin untuk mengubah hak akses pengguna tersebut?</p>
 
                     <form id="change-user-role-form" action="{{ route('administrator.change.user.role') }}"
-                          method="post" enctype="multipart/form-data"
-                          class="d-none">
+                        method="post" enctype="multipart/form-data" class="d-none">
                         @csrf
 
                         <input type="hidden" id="userEmail" name="userEmail">
@@ -157,7 +160,7 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
                     <button type="button" class="btn btn-warning"
-                            onclick="event.preventDefault(); document.getElementById('change-user-role-form').submit();">
+                        onclick="event.preventDefault(); document.getElementById('change-user-role-form').submit();">
                         Lanjutkan
                     </button>
                 </div>
@@ -168,7 +171,7 @@
 
 @section('script')
     <script type="text/javascript">
-        $('#resetPassword').on('show.bs.modal', function (event) {
+        $('#resetPassword').on('show.bs.modal', function(event) {
             var button = $(event.relatedTarget)
             var email = button.data('email');
 
@@ -176,7 +179,7 @@
             modal.find('.modal-body #userEmail').val(email)
         })
 
-        $('#changeUserRole').on('show.bs.modal', function (event) {
+        $('#changeUserRole').on('show.bs.modal', function(event) {
             var button = $(event.relatedTarget)
             var email = button.data('email');
 
